ed5f5aa06905cc9e739dc930e9e606b53bd252f9
DeRampage <philipplplimbacho@web.de>
2016-04-22 15:35:54 +0200

    fuckCents
    -fix for 4

--- zins.c
+++ zins.c
@@ -3,6 +3,8 @@
 float CountCentsFloat(unsigned int betrag);
 
 int main(void){
+	/* Protokollblatt
+	1. und 2.)
 	
 	//Werte fuer sg
 	// Fuer 0.01 = 0a d7 23 3c
@@ -11,6 +13,10 @@ int main(void){
 	// Werte fuer dg
 	// Fuer 0.01 = 7b 14 ae 47 e1 7a 84 3f
 	// Fuer 0.25 = 00 00 00 00 00 00 d0 3f
+	
+	5.)
+	153
+	*/
 	float sg = 0.25; //einfache Genauigkeit
 	double dg = 0.25; //doppelte Genauigkeit
 	
@@ -23,8 +29,13 @@ int main(void){
 }
 
 float CountCentsFloat(unsigned int betrag) {
+	int b = 0;
+	while (b < (int) betrag) {
+		b++;
+	}
 	float cent = 0.0f;
-	while (cent < betrag) {
+	int i;
+	for(i = 0; i < b * 100; i++) {
 		cent = cent + 0.01;
 	}
 	return cent;
