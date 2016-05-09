c4fe19b57eca2a0c0a4a34df1e60efc680d1bda5
DeRampage <philipplplimbacho@web.de>
2016-05-09 18:30:15 +0200

    finish

--- mandel.c
+++ mandel.c
@@ -1,15 +1,20 @@
 /* Praktikum Maschinenorientiertes Programmieren - Vorlage Versuch 4 von D. Pawelczak */
+#include <math.h>
 #ifdef MOPVM
 #include <sdlite.h>
 #else
 #include <SDL/sdl.h>
 #endif
 
+/* Konstanten */
 #define SIZE_X 320
 #define SIZE_Y 240
 #define MAX_ITERATION 128
 #define BIT_DEPTH 32
+#define MAX_BETRAG 4
+#define EINEZEILE SIZE_X * 4
 
+/* Strukturen */
 typedef struct{
 	unsigned char blue; 
 	unsigned char green;
@@ -17,10 +22,17 @@ typedef struct{
 	unsigned char alpha;
 } tColor;
 
+typedef struct {
+	double re;
+	double im;
+} tComplex;
+
+/*Funktionsprototypen*/
 void PutPixel(unsigned char* videoRam, int x, int y, const tColor *c);
 void FillScreen(unsigned char* videoRam, const tColor *c);
+int Iteration(tComplex *c);
+void Mandelbrot (unsigned char* videoRam, tComplex *c, double delta);
 
-/* ToDo: define tColor */
 const tColor gColorList[] = 
 { 	
 	{ 0x10, 0x00, 0x00, 0xFF }, { 0x20, 0x00, 0x00, 0xFF }, { 0x30, 0x00, 0x00, 0xFF }, { 0x40, 0x00, 0x00, 0xFF }, { 0x50, 0x00, 0x00, 0xFF }, { 0x60, 0x00, 0x00, 0xFF }, { 0x70, 0x00, 0x00, 0xFF }, { 0x80, 0x00, 0x00, 0xFF },
@@ -44,26 +56,50 @@ const tColor gColorList[] =
 
 int main(int argc, char** argv) {
 	
-	tColor sandyBrown = { .blue = 0x60, 
-		.green = 0xA4, .red = 0xF4, .alpha = 0xFF};
-	
 	unsigned char* videoRam = NULL;
 	SDL_Surface *screen = NULL;
 
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	if (SDL_Init(SDL_INIT_VIDEO) != 0){
 		return 1;
+	}
     screen = SDL_SetVideoMode(SIZE_X, SIZE_Y, BIT_DEPTH, 0);
-    if (screen == NULL)
+    if (screen == NULL){
         return 2;
+	}
 	/* the ptr to video memory */
 	videoRam = screen->pixels;
-	PutPixel(videoRam, 50, 50, &sandyBrown);
-	FillScreen(videoRam, &sandyBrown);
-	printf("%i", videoRam[0]);
-	printf("\n%i", videoRam[1]);
-	printf("\n%i", videoRam[2]);
-	printf("\n%i", videoRam[3]);
-	/* Todo */
+	
+	// Aufgabe 1
+	tColor sandyBrown = { .blue = 0x60, .green = 0xA4, .red = 0xF4, .alpha = 0xFF};
+	//PutPixel(videoRam, 50, 50, &sandyBrown);
+	
+	// Aufgabe 2
+	//FillScreen(videoRam, &sandyBrown);
+	
+	//Aufgabe 3
+	/*
+	tComplex ceins = {0, 0}; // Antwort 128
+	tComplex czwo = {0.5, 0}; // Antwort 5
+	tComplex cdrei = {-14, 17}; // Antwort 0
+	printf("\nAnzahl Wiederholung: %i", Iteration(&czwo));
+	*/
+	
+	//Aufgabe 5
+	/*
+	tComplex ctest = {-2, -1};
+	double delta = 0.008;
+	*/
+	
+	// Aufgabe 6
+	/*
+	tComplex ctest = {0.410600057, 0.3616605002};
+	double delta = 0.0000004;
+	*/
+	
+	//Mandelbrot(videoRam, &ctest, delta);
+	
+	/* Zusatzaufgabe*/
+	
 	SDL_Flip(screen);
 	return 0;
 }
@@ -73,8 +109,8 @@ void PutPixel(unsigned char* videoRam, int x, int y, const tColor *c) {
 		x = x * 4;
 		if(y >= 0 && y < SIZE_Y){
 			if(y > 0){
-				for(y;y>0;y--){
-					x+=1280;
+				for(y = y; y > 0; y--){
+					x+= EINEZEILE;
 				}
 			}
 			videoRam[x] = c->blue;
@@ -95,5 +131,34 @@ void FillScreen(unsigned char* videoRam, const tColor *c) {
 			y++;
 		}
 		x++;
+	}
+}
+
+int Iteration(tComplex *c) {
+	tComplex zAlt = {0, 0};
+	tComplex zNeu = {0, 0};
+	int i = 0;
+	for(i = 0; i < MAX_ITERATION; i++) {
+		double real = (pow(zAlt.re, 2) - pow(zAlt.im, 2)) + c->re;
+		double imag = (2 * zAlt.re * zAlt.im) + c->im;
+		if(pow(real, 2) + pow(imag, 2) > pow(MAX_BETRAG, 2)) break;
+		zNeu.re = real;
+		zNeu.im = imag;
+		zAlt.re = zNeu.re;
+		zAlt.im = zNeu.im;
+	}
+	return i;
+}
+
+void Mandelbrot (unsigned char* videoRam, tComplex *c, double delta) {
+	double start = c -> re;
+	for(int i = 0; i < SIZE_Y; i++) {
+		c->re = start;
+		for(int k = 0; k < SIZE_X; k++) {
+			tColor color = gColorList[Iteration(c)];
+			PutPixel(videoRam, k, i, &color);
+			c->re += delta;
+		}
+		c->im += delta;
 	}
 }
