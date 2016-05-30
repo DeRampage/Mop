e4b2086eed5c5c5085473a007a9ec94fc37a28b2
RampageTf47 <philipp.loebel1@gmail.com>
2016-05-30 11:55:29 +0200

    completed Task 2:  dropchip()

--- VierGewinnt/main.c
+++ VierGewinnt/main.c
@@ -1,10 +1,32 @@
 #include "playground.h"
-#define MAX_X 4
-#define MAX_Y 4
+#define MAX_X 5
+#define MAX_Y 5
+
+int DropChip(tPlayer array[], int col, tPlayer chip) {
+	if(array[(MAX_Y - 1) * MAX_X - 1 + col] != 0){
+		return 0; //Spalte ist voll
+	} else {
+		array[(MAX_Y - 1) * MAX_X + col] = chip;
+		DrawPlayGround(array, MAX_X, MAX_Y);
+		int y = 1;
+		while(y < MAX_Y && array[(MAX_Y - (y + 1)) * MAX_X + col] == 0) {
+			array[(MAX_Y - y) * MAX_X + col] = 0;
+			y++;
+			array[(MAX_Y - y) * MAX_X + col] = chip;	
+			DrawPlayGround(array, MAX_X, MAX_Y);	
+		}
+		return 1; //Erfolg
+	}
+}
 
 int main(int argc, char **argv)
 {
+	enum tPlayer playGround[MAX_X * MAX_Y] = {0};
 	InitPlayGround(MAX_X, MAX_Y);
-
+	/* Testarea	
+	playGround[1] = 1;
+	*/
+	DrawPlayGround(playGround, MAX_X, MAX_Y);
+	DropChip(playGround, SelectCol(), PLAYER_R);
 	return 0;
 }
