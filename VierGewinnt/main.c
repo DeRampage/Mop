2ea48045fcca3166ad96dc1656de817a5e6a8a8d
RampageTf47 <philipp.loebel1@gmail.com>
2016-05-30 17:19:19 +0200

    WIP CheckHor

--- VierGewinnt/main.c
+++ VierGewinnt/main.c
@@ -36,21 +36,35 @@ int CheckComplete(tPlayer array[]) {
 }
 
 int CheckHor (const tPlayer array[], tPlayer player){
+	//INIT
+	int max_verschiebung = MAX_X - 4;
 	int x = 0;
 	int y = 0;
 	int won = 0;
+	
+	//Check
 	while(won != 1) {
-		if(array[x] == player){
-			if(array[x + 1] == player){
-				if(array[x + 2] == player){
-					if(array[x + 3] == player){
-						won++;
-					}
-				}		
-			}	
+		for(x = 0; x <= max_verschiebung; x++) {//stellt sicher dass 4 nicht ¸ber zwei Zeilen geht
+			for(y = 0; y <= MAX_Y; y++) {//stellt sicher dass 4 nicht ¸ber zwei Zeilen geht
+				if(array[x + y] == player){
+					if(array[x + y + 1] == player){
+						if(array[x + y + 2] == player){
+							if(array[x + y + 3] == player){
+								won++;
+							}
+						}		
+					}	
+				}
+			}
 		}
-	y++;
-	x = x + y;
+	}
+	
+	//Result
+	if(won > 0) {
+		ShowYouWon(player);
+		return 1;
+	} else {
+		return 0;
 	}
 }
 
@@ -65,7 +79,7 @@ int main(int argc, char **argv)
 	playGround[MAX_Y * MAX_X] = 1; //Auﬂerhalb der Grenzen
 	*/
 	DrawPlayGround(playGround, MAX_X, MAX_Y);
-	while (CheckComplete(playGround) == 0) {
+	while (CheckComplete(playGround) == 0 /*&& CheckHor(playGround, 1) == 0 && CheckHor(playGround, 2) == 0*/) {
 		DropChip(playGround, SelectCol(), PLAYER_R);
 	}
 	return 0;
