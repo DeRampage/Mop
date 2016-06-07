a8ce0a373d7d95da3f09145a604acee61a4afcaf
RampageTf47 <philipp.loebel1@gmail.com>
2016-06-07 11:40:20 +0200

    1&2 Task

--- ListeText.c
+++ ListeText.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct {
-	
+typedef struct list {
+	struct list *next;
+	char* data[1];
 } tList;
 
+tList* neu = malloc (sizeof(tList));
+
+tList * CreateWord (char * word) {
+	neu->next = 'NULL';
+	neu->data = word;
+	return neu;	
+}
+
+tList* AddElement (tList* head, char* word) {
+	if(head == NULL) {
+		tList* neu = calloc(1, sizeof(tList));
+		neu->next = NULL;
+		neu->data = word;
+		return neu->data;
+	} else {
+			
+	}
+}
+
 int main(void){
-	
+	char* bimbam = CreateWord("Hallo");
+	/*
+	tList* head = AddElement(NULL, "Kopf");
+	AddElement(AddElement(head, "bis"), "Fuss");
+	*/
 	return 0;
 }
