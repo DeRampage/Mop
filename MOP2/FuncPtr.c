8d5fcc2a6946edf5ef2009de67f723dae62b5ac3
DeRampage <philipplplimbacho@web.de>
2016-10-13 10:17:57 +0200

    Mop2 Start

---
+++ MOP2/FuncPtr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int addInt (int n, int m);
+
+int main(void){
+	
+	printf("Die Zahl ist: %d\n", 1);
+}
+int addInt(int n, int m) {
+    return n+m;
+}
